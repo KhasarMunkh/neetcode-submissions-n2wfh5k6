521c41aaa9e1c040d634af407c13fcf2b395a679
Khasar Munkh-Erdene <02khasar@gmail.com>
2026-04-30 18:27:40 -0600

    Add: ipo - submission-3

---
+++ Data Structures & Algorithms/ipo/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    // at most k distinct projects
+    // w capital
+    // capital[i] required to start project
+    // profits[i] added to capital after project
+    // return maximizedCapital
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        std::priority_queue<std::pair<int,int>, std::vector<std::pair<int,int>>, std::greater<std::pair<int,int>>> minHeap;
+        priority_queue<int> maxProfitsHeap;
+        for (int i = 0; i < profits.size(); i++) {
+            minHeap.push({capital[i],profits[i]});
+        }
+        // k times, do a project
+        for (int i = 0; i < k; i++) {
+            if (minHeap.empty() && maxProfitsHeap.empty()) {
+                break;
+            }
+            // collect the projects we can do with our current capital
+            // and push the profits into a maxHeap
+            while (w >= minHeap.top().first && !minHeap.empty()) {
+                int profit = minHeap.top().second;
+                minHeap.pop();
+                maxProfitsHeap.push(profit);
+            }
+            if (!maxProfitsHeap.empty()) {
+                w += maxProfitsHeap.top();
+                maxProfitsHeap.pop();
+            }
+        }
+        return w;
+    }
+};
